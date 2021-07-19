e42f168010e6d9aa5c62d96418bf71a0071c48b6
TsakBoolhak <tsakboolhak@gmail.com>
2021-07-19 04:48:03 +0200

    finished parsing and added some error map files

--- srcs/main.c
+++ srcs/main.c
@@ -3,19 +3,26 @@
 #include <fcntl.h>
 #include "libft.h"
 
+typedef struct s_coord
+{
+	int	x;
+	int	y;
+}t_coord;
+
 typedef struct s_game
 {
 	int		height;
 	int		width;
 	char	**map;
+	t_coord	player;
 }t_game;
 
 typedef struct s_parse
 {
 	int	exit;
-	int	start;
 	int	collectible;
-	int	error;
+	int	line_error;
+	int	char_error;
 }t_parse;
 
 void	print_usage(void)
@@ -31,16 +38,28 @@ void	print_map_rules(void)
 {
 	ft_putendl_fd("\nMap rules:", 2);
 	ft_putstr_fd("- Map must be composed of only 5 possible characters", 2);
-	ft_putstr_fd(": '0'(zero) for an empty space, '1' (one) for a wall, ", 2);
+	ft_putstr_fd(": '0' (zero) for an empty space, '1' (one) for a wall, ", 2);
 	ft_putstr_fd("'C' for a collectible, 'E' for map exit and ", 2);
 	ft_putendl_fd("'P' for the player's starting position.", 2);
 	ft_putendl_fd("- Map must be closed/surrounded by walls", 2);
-	ft_putendl_fd("- Map must contain at least 1 exit and 1 collectible", 2);
-	ft_putendl_fd("- Map must be rectangular", 2);
+	ft_putstr_fd("- Map must contain at least one exit and one ", 2);
+	ft_putendl_fd("collectible\n- Map must be rectangular", 2);
 	ft_putstr_fd("- Map must contain one and only one ", 2);
 	ft_putendl_fd("player's starting position\n", 2);
 	ft_putendl_fd("Here is an exemple of a simple valid map", 2);
 	ft_putendl_fd("1111111\n1C01EP1\n10C11C1\n1C00001\n1111111", 2);
+}
+
+void	print_map(char **map, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		ft_putendl_fd(map[i], fd);
+		i++;
+	}
 }
 
 char	**ft_add_str_to_tab(char **tab, char *line)
@@ -86,39 +105,152 @@ int	check_argument(int ac, char **av)
 	return (0);
 }
 
-void	print_line_error(char *line, int row, int *no_print)
+void	print_line_error(char *line, int row, int *no_print, char **map)
 {
 	if (!(*no_print))
 	{
-		ft_putstr_fd("Error\nMap is not valid\n\nError occured in line #", 2);
+		ft_putstr_fd("Error\nMap is not valid\n", 2);
+		ft_putendl_fd("\nHere is the parsed map when the error occured:\n", 2);
+		print_map(map, 2);
+		ft_putstr_fd("\nError occured in line #", 2);
 		ft_putnbr_fd(row, 2);
 		ft_putstr_fd(":\n\"", 2);
 		ft_putstr_fd(line, 2);
-		ft_putendl_fd("\"", 2);
+		ft_putendl_fd("\"\n", 2);
 		*no_print = -1;
 	}
 }
 
-int	check_line(char **line, t_game *game, t_parse *parse)
+void	print_char_error(char c, int column, int *flag)
+{
+	if (!(*flag))
+	{
+		ft_putstr_fd("-Wrong character : ", 2);
+		if (ft_isprint(c))
+		{
+			ft_putstr_fd("'", 2);
+			write(2, &c, 1);
+			ft_putstr_fd("'", 2);
+		}
+		ft_putstr_fd("\t", 2);
+		ft_putstr_fd("(ASCII value: ", 2);
+		ft_putnbr_fd((int)c, 2);
+		ft_putstr_fd(")\ton col #", 2);
+		ft_putnbr_fd(column, 2);
+		ft_putstr_fd("\t:", 2);
+		*flag = -1;
+	}
+}
+
+void	handle_valid_char(char c, int i, t_game *game, t_parse *parse)
+{
+	if (c == 'P')
+	{
+		game->player.x = i;
+		game->player.y = game->width - 1;
+	}
+	else if (c == 'C')
+		(parse->collectible)++;
+	else if (c == 'E')
+		(parse->exit)++;
+}
+
+void	check_char(char *line, int i, t_game *game, t_parse *parse)
+{
+	if (line[i] != '1' && (!i || game->height == 1 || i == game->width - 1))
+	{
+		print_line_error(line, game->height, &parse->line_error, game->map);
+		print_char_error(line[i], i + 1, &parse->char_error);
+		ft_putstr_fd(" -Map is not closed", 2);
+	}
+	if (!ft_strchr("01ECP", line[i]))
+	{
+		print_line_error(line, game->height, &parse->line_error, game->map);
+		print_char_error(line[i], i + 1, &parse->char_error);
+		ft_putstr_fd(" -Not a valid character", 2);
+	}
+	if (line[i] == 'P' && game->player.x != -1)
+	{
+		print_line_error(line, game->height, &parse->line_error, game->map);
+		print_char_error(line[i], i + 1, &parse->char_error);
+		ft_putstr_fd(" -Map contain more than one player spawn", 2);
+	}
+	handle_valid_char(line[i], i, game, parse);
+	if (parse->char_error)
+		ft_putendl_fd("", 2);
+}
+
+int	check_line(char *line, t_game *game, t_parse *parse)
 {
 	int	i;
 	int	len;
 
 	i = 0;
-	len = ft_strlen(*line);
+	len = ft_strlen(line);
 	if (len < 3)
 	{
-		print_line_error(*line, game->height, &parse->error);
+		print_line_error(line, game->height, &parse->line_error, game->map);
 		ft_putendl_fd("-Line is too short to contain a closed map", 2);
 	}
 	if (!game->width)
 		game->width = len;
 	else if (len != game->width)
 	{
-		print_line_error(*line, game->height, &parse->error);
+		print_line_error(line, game->height, &parse->line_error, game->map);
 		ft_putendl_fd("-Map is not rectangular", 2);
 	}
-	return (parse->error);
+	while (line[i])
+	{
+		parse->char_error = 0;
+		check_char(line, i, game, parse);
+		i++;
+	}
+	return (parse->line_error);
+}
+
+void	print_last_error(t_parse *parse, char **map, int no_spawn)
+{
+	if (!parse->line_error)
+	{
+		ft_putendl_fd("Error\nMap is not valid", 2);
+		ft_putendl_fd("\nHere is the parsed map when the error occured:\n", 2);
+		print_map(map, 2);
+		parse->line_error = -1;
+	}
+	if (!parse->exit)
+		ft_putendl_fd("\n-Map has no exit", 2);
+	if (!parse->collectible)
+	{
+		if (parse->exit)
+			ft_putendl_fd("", 2);
+		ft_putendl_fd("-Map has no collectible", 2);
+	}
+	if (no_spawn == -1)
+	{
+		if (parse->exit && parse->collectible)
+			ft_putendl_fd("", 2);
+		ft_putendl_fd("-Map has no spawn", 2);
+	}
+}
+
+void	last_parsing_checks(char *line, t_game *game, t_parse *parse)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		parse->char_error = 0;
+		if (line[i] != '1')
+		{
+			print_line_error(line, game->height, &parse->line_error, game->map);
+			print_char_error(line[i], i + 1, &parse->char_error);
+			ft_putendl_fd(" -Map is not closed", 2);
+		}
+		i++;
+	}
+	if (!parse->collectible || !parse->exit || game->player.x == -1)
+		print_last_error(parse, game->map, game->player.x);
 }
 
 int	map_parser(int fd, t_game *game, t_parse *parse)
@@ -131,30 +263,31 @@ int	map_parser(int fd, t_game *game, t_parse *parse)
 	{
 		ret = get_next_line(fd, &line, 0);
 		if (ret == -1)
-		{
-			get_next_line(fd, &line, 2);
 			ft_putendl_fd("Error\nError occured while reading file.", 2);
+		if (ret == -1)
 			return (-1);
-		}
 		else if (!ret)
-			break;
+			break ;
 		game->map = ft_add_str_to_tab(game->map, line);
 		(game->height)++;
-		if (check_line(&line, game, parse))
-		{
-			get_next_line(fd, &line, 2);
-			break;
-		}
+		if (check_line(line, game, parse))
+			return (-1);
 	}
-	return (parse->error);
+	if (!game->height)
+	{
+		ft_putendl_fd("Error\nFile is empty", 2);
+		return (-1);
+	}
+	last_parsing_checks((game->map)[game->height - 1], game, parse);
+	return (parse->line_error);
 }
 
 void	init_parsing(t_game *game, t_parse *parse)
 {
-	game->height = 0;
-	game->width = 0;
-	game->map = NULL;
 	ft_memset(parse, 0, sizeof(*parse));
+	ft_memset(game, 0, sizeof(*game));
+	game->player.x = -1;
+	game->player.y = -1;
 }
 
 int	parser(int ac, char **av, t_game *game)
@@ -175,6 +308,7 @@ int	parser(int ac, char **av, t_game *game)
 	init_parsing(game, &parse);
 	if (map_parser(fd, game, &parse))
 	{
+		get_next_line(fd, NULL, 2);
 		close(fd);
 		ft_free_tab((void **)(game->map));
 		print_map_rules();
@@ -186,68 +320,9 @@ int	parser(int ac, char **av, t_game *game)
 int	main(int ac, char **av)
 {
 	t_game	game;
-/*	int		fd;
-	char	*line;
-	int		ret;
-	int		i;
-	int		empty;
-	int		starting_spaces;
-*/
+
 	if (parser(ac, av, &game))
 		return (0);
-/*	ret = 1;
-	empty = 0;
-	starting_spaces = -1;
-	game.width = -1;
-	line = NULL;
-	while (ret > 0)
-	{
-		ret = get_next_line(fd, &line, 0);
-		if (ret == -1)
-		{
-			ft_putendl_fd("Error\nError occured while reading file.", 2);
-			close(fd);
-			return (0);
-		}
-		else if (!ret)
-			break;
-		i = 0;
-		ft_putendl_fd(line, 1);
-		while (line[i] && line[i] != ' ')
-		{
-			if (!ft_strchr("01ECP", line[i]))
-			{
-				free(line);
-		    	get_next_line(fd, &line, 2);
-				ft_putendl_fd("Error\nFile's map contain invalid character", 2);
-				close(fd);
-				return (0);
-			}
-			i++;
-		}
-		if (game.width == -1)
-			game.width = i - starting_spaces;
-		else if (game.width != i - starting_spaces)
-		{
-		    get_next_line(fd, &line, 2);
-			free(line);
-			ft_putendl_fd("Error\nFile's map isn't rectangular", 2);
-			close(fd);
-			return (0);
-		}
-		while (line[i] == ' ')
-			i++;
-		if (line[i])
-		{
-		    get_next_line(fd, &line, 2);
-			free(line);
-			ft_putendl_fd("Error\nFile contains more than one map", 2);
-			close(fd);
-			return (0);
-		}
-		free(line);
-	}
-	close(fd);*/
 	ft_free_tab((void **)(game.map));
 	return (0);
 }
